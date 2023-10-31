513d3cf4847b239ebb23134dda31036a79794983
Abdulrehman-devops <143611927+Abdulrehman-devops@users.noreply.github.com>
2023-10-31 14:11:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
-const mongoSanitize = require("express-mongo-sanitize")
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const mongoSanitize = require("express-mongo-sanitize");
 
-// start app
-const app = express()
+const app = express();
+const port = process.env.PORT || 5000;
 
-// Express port
-const port = process.env.PORT || 5000
-// const db = process.env.DATABASE
+// Enable CORS for specific origins or "*" to allow all origins
+app.use(cors({
+  origin: "https://yourfrontendapp.com", // Replace with your frontend app's URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
 
-
-app.use(cors())
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'client', 'build')))
+// Serve static files from the "client/build" directory
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Sanitize against NoSQL query injections
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
+app.get("/api/", (req, res) => {
+  return res.status(200).json({
+    status: "success",
+  });
+});
 
-app.get('/api/', (req, res) => {
-    return res.status(200).json({
-        status: "success"
-    })
-})
-
-// Redirect back to index.html if urls do not match
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"))
-})
-
-
+  // This handles routes that do not match. Make sure to serve your SPA's index.html.
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
-
+  console.log(`Listening on port ${port}`);
+});
