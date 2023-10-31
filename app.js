5095f72fd06a183c7a2d8dd1104c006c831f9913
Abdulrehman-devops <143611927+Abdulrehman-devops@users.noreply.github.com>
2023-10-31 13:25:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ app.use(express.static(path.join(__dirname, 'client', 'build')))
 app.use(mongoSanitize())
 
 // Setting up a route for our API
-app.get('/api/', (req, res) => {
-    return res.status(200).json({
-        status: "success"
-    })
+//app.get('/api/', (req, res) => {
+  //  return res.status(200).json({
+    //    status: "success"
+    //})
 })
 
 // Redirect back to index.html if urls do not match
